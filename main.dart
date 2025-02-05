94b1525deb455e31ead08fb45f4e56152fa1c34f
Shakib Ahamed Tushar <47601402+ShakibXAT@users.noreply.github.com>
2025-02-05 23:20:50 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AddEmployeeScreen(),
+    );
+  }
+}
+
+class AddEmployeeScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController salaryController = TextEditingController();
+
+  void submitForm() {
+    if (_formKey.currentState!.validate()) {
+      String name = nameController.text;
+      String age = ageController.text;
+      String salary = salaryController.text;
+
+      print("Employee Added: Name: $name, Age: $age, Salary: $salary");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Light background color
+      appBar: AppBar(
+        title: Text("Add Employee"),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: "Name"),
+                validator: (value) => value!.isEmpty ? "Enter Name" : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: ageController,
+                decoration: InputDecoration(labelText: "Age"),
+                keyboardType: TextInputType.number,
+                validator: (value) => value!.isEmpty ? "Enter Age" : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: salaryController,
+                decoration: InputDecoration(labelText: "Salary"),
+                keyboardType: TextInputType.number,
+                validator: (value) => value!.isEmpty ? "Enter Salary" : null,
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: submitForm,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.purple,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text("Add Employee"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
